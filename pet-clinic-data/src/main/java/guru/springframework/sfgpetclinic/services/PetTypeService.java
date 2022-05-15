0d3adcecb4c2bf9f3c38f5c6dd5634703b9804bd
Ihor Horbonos <igorgorb@gmail.com>
2022-05-15 19:01:34 +0300

    added PetTypeMapService. Closes #30

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author igorg
+ * Date 15.05.2022
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
