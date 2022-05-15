5c08f769d020b3fb0a613a6b0939f83d540331e0
Ihor Horbonos <igorgorb@gmail.com>
2022-05-15 18:31:20 +0300

    added visits, updated with pet relationship. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author igorg
  * Date 02.05.2022
  */
-public class Owner extends Person{
+public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
