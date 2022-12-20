6763b3f61893925ca0fdbe7bd7d11856f613010f
Ihor Horbonos <igorgorb@gmail.com>
2022-12-20 22:49:22 +0200

    Adding builder pattern to PetType. #57

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -7,17 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 /**
- * @author igorg 
+ * @author igorg
  * @data 02.05.2022
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
+
+  @Builder
+  public PetType(String name, Long id) {
+    super(id);
+    this.name = name;
+  }
 
   @Column(name = "name")
   private String name;
